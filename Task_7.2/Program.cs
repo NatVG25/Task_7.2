523cb874d76ab2f18720bb90905bd9ceb28c5b82
Goliash <Goliash@KONSTRUKTOR4>
2021-11-29 13:43:58 +0500

    Добавьте файлы проекта.

---
+++ Task_7.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //вводим исходные параметры
+            Console.WriteLine("Введите длину стороны куба:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V; //инициализация переменных
+            CalcCube(a, out S, out V); 
+            Console.WriteLine("Для куба со стороной {0} Объем V равен {1,0:00}, площадь поверхности S равна {2,0:00}", a, V, S);//вывод результата
+            Console.ReadKey();
+
+        }
+        static void CalcCube(int a, out double S, out double V) //метод расчета объема и площади поверхности куба 
+        {
+            V = (double)Math.Pow(a, 3); //объем
+            S = 6 * Math.Pow(a, 2); //площадь поверхности
+        }
+    }
+}
